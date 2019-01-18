e2cbf8ade75d8377c1e6d600a4f03ab1b04cffea
Toni-Ty <antonique2479@yahoo.com>
2019-01-18 00:24:07 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -5,13 +5,6 @@ require 'pry'
 collection = (1..100). to_a
 my_find(collection) { |i| i % 3 == 0 and i % 5 == 0}
 
-def my_find(collection)
-  i = 0
-  while i < 100
-    yield (1)
-      i = i + 1
-  end
-end
 
 
 
